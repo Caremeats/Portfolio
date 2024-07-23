e746f46e933ead5f3ac4cfbbcfd6659960a91c07
Erryl <erryljames@gmail.com>
2024-07-23 17:41:09 +0800

    w

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
@@ -14,21 +15,19 @@ const Hero = () => {
       </div>
 
       
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
       </div>
 
       <div className="flex-justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+        <div className="max-w-full md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Web Developer</h2>
-
           <TextGenerateEffect className="text-center text-[40px] md:text-5x lg:text-6xl" words="Erryl James Vargas"/>
-
         </div>
 
         <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi! I am a Graduate in Computer Enginering from Adamson </p>
 
-        <a href="#about"> <MagicButton title="Show my Work"/></a>
+        <a href="#about"> <MagicButton title="Show my Work" icon={<FaLocationArrow/>} position='right'/></a>
 
       </div>
 
