5acbbd68b2a4a282e6cfeab0d19be11a65735ea9
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-22 19:10:14 +0800

    Fix flex justify-center

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,17 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Footer from "@/components/Footer";
- 
+
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex-justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
-        <FloatingNav  navItems={navItems}/>
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        <RecentProjects/>
-        <Footer/>
-
-
+        <RecentProjects />
+        <Footer />
       </div>
-
     </main>
   );
 }
