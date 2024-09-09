ad6f41bc4719fca7c2544727f63845b82d56b4e9
Erryl James G. Vargas <erryljames@gmail.com>
2024-09-09 18:15:44 +0800

    Trying to fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((profile) => (
-            <div
+            <a
               key={profile.id}
               href={profile.link}
               target="_blank"
@@ -40,7 +40,7 @@ const Footer = () => {
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blue-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={profile.img} width={20} height={20} />
-            </div>
+            </a>
           ))}
         </div>
       </div>
